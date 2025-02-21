f37201c8fbead6211b27ff03520d80119e26159a
muratozcan-seon <murat.ozcan@seon.io>
2025-02-21 09:50:53 -0600

    feat: py refinements

--- 09-conditionals-operators.py
+++ 09-conditionals-operators.py
@@ -1,15 +1,16 @@
 inflation_september = 3.2
 inflation_august = 3.5
 
+
 # Check if September inflation is less than August inflation
 if inflation_september < inflation_august:
 	print('Inflation decreased')
-else:
+elif inflation_september > inflation_august:
 	print('Inflation increased')
+else:
+	print('Inflation remained stable')
 
 #########
-
-
 
 # Define your requirements
 min_num_beds = 2
@@ -22,11 +23,15 @@ sq_foot = 800
 rent = 1800
 
 
+# Check the number of beds
 if num_beds < min_num_beds:
-	print('Too few beds')
-elif sq_foot < min_sq_foot:
-	print('Too small square foot')
-elif rent > max_rent:
-	print('Rent is too high')
+  print('Insufficient bedrooms')
+# Check square feet
+if sq_foot <= min_sq_foot:
+  print('Too small')
+# Check the rent
+if (rent > max_rent):
+  print('Too expensive')
 else:
-	print('Property meets requirements')
+  print('This looks promising!')
+  
--- 11-while-loops.py
+++ 11-while-loops.py
@@ -14,17 +14,17 @@ current_date = 22
 
 # Create a conditional loop while current_date is less than or equal to release_date
 while current_date <= release_date:
-    # Promote purchases before the 25th
-    if current_date <= 24:
-        print("Purchase before the 25th for early access")
-    
-    # Check if the date is exactly the 25th
-    elif current_date == 25:
-        print("Coming soon!")
-    
-    # All other dates (26th and beyond)
-    else:
-        print("Available now!")
-    
-    # ✅ Ensure current_date increments to avoid an infinite loop
-    current_date += 1
+	# Promote purchases before the 25th
+	if current_date <= 24:
+			print("Purchase before the 25th for early access")
+	
+	# Check if the date is exactly the 25th
+	elif current_date == 25:
+			print("Coming soon!")
+	
+	# All other dates (26th and beyond)
+	else:
+			print("Available now!")
+	
+	# ✅ Ensure current_date increments to avoid an infinite loop
+	current_date += 1
