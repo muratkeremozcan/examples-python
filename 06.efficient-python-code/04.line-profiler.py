9c0a88420f4e2d0bcb024589ee0cd946183a9e40
Murat Ozcan <murat@mac.lan>
2025-04-15 07:45:02 -0500

    feat: mem preofiler

--- 06.efficient-python-code/04.line-profiler.py
+++ 06.efficient-python-code/04.line-profiler.py
@@ -1,11 +1,9 @@
+# %load_ext line_profiler
+# %lprun -f fn-name fn-name(args)
+
+# using %timeit in multi-line code is inefficient, so we use code-profiling
 import numpy as np
 
-# using %timeit in multi-line code is inefficient
-# so we use code-profiling
-
-# %load_ext line_profiler
-# command for line-by-line 
-# %lprun -f fn-name fn-name(args)
 
 def convert_units(heroes, heights, weights):
 
@@ -24,8 +22,23 @@ heroes = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman"]
 hts = np.array([191, 183, 182, 175, 188])
 wts = np.array([107, 95, 75, 70, 90])
 
-# to profile this code, you would:
+# 1. Load the memory profiler extension in your IPython or Jupyter session:
+#       %load_ext memory_profiler
+# 2. Run the profiler on the convert_units() function:
+#      %lprun -f convert_units convert_units(heroes, hts, wts)
 
-# Run the code in an IPython environment (like Jupyter Notebook)
-# to load the extension: %load_ext line_profiler 
-#  to see the detailed line-by-line profiling results:  %lprun -f convert_units convert_units(heroes, hts, wts)
+
+# output:
+# Timer unit: 1e-06 s
+
+# Total time: 32 µs
+# File: <ipython-input-1>, function convert_units
+# Line #      Hits         Time  Per Hit   % Time  Line Contents
+# ==============================================================
+#      3                                           def convert_units(heroes, heights, weights):
+#      4         1         7 µs      7 µs    21.88      new_hts = [ht * 0.39370 for ht in heights]
+#      5         1         8 µs      8 µs    25.00      new_wts = [wt * 2.20462 for wt in weights]
+#      6         1         4 µs      4 µs    12.50      hero_data = {}
+#      7         1         7 µs      7 µs    21.88      for i, hero in enumerate(heroes):
+#      8         1         6 µs      6 µs    18.75          hero_data[hero] = (new_hts[i], new_wts[i])
+#      9         1         0 µs      0 µs     0.00      return hero_data
